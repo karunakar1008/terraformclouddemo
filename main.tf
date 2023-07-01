5cb5ec6fa6d3adc549cecefd43a0a0210ecb5744
karunakar bhogyari <karunakar1008@gmail.com>
2023-07-01 20:32:25 +0530

    variables remove

--- main.tf
+++ main.tf
@@ -1,13 +1,9 @@
 provider "azurerm" {
   features {}
-
-  client_id       = var.ARM_CLIENT_ID
-  client_secret   = var.ARM_CLIENT_SECRET
-  tenant_id       = var.ARM_TENANT_ID
-  subscription_id = var.ARM_SUBSCRIPTION_ID
 }
 
 terraform {
+  required_version = "~> 1.0"
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
